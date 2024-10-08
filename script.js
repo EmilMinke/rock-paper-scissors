309aa8c8d9c45b26ea3686e0015a459635c5a974
EmilMinke <em.minke@gmail.com>
2024-10-08 20:58:22 +0200

    feat: Clean up comments and console.log tests

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// CREATE a variable that stores a random whole number between 1 and 100.
-// CREATE a function that returns a string of either "Rock", "Paper" or "Scissors"
-// which depends on the stored number.
-// CREATE a function that takes a user input and returns it.
-// The user input is their choice between "Rock", "Paper" and "Scissors"
-// CREATE two variables for human score and computer score and initialize them with the value of 0;
-
 const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSORS = "Scissors";
@@ -27,8 +20,6 @@ function getHumanChoice(){
     let userInput = prompt("Rock, Paper or Scissors?");
     return userInput;
 }
-
-console.log(getHumanChoice());
 
 // Plan or pseudocode your solution.
 // Write the code.
