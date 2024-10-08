47ab4278aef87ce99d0d0bab8d52f03e2dc43642
EmilMinke <em.minke@gmail.com>
2024-10-08 20:17:11 +0200

    feat: Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-console.log("hello World")
+// CREATE a variable that stores a random whole number between 1 and 100.
+// CREATE a function that returns a string of either "rock", "paper" or "scissors"
+// which depends on the stored number.
+
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+function getComputerChoice() {
+    if (randomNumber <= 33) {
+        return "Rock"
+    } else if (randomNumber <= 66 && randomNumber > 33) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
 
 // Plan or pseudocode your solution.
 // Write the code.
 // Test your code to make sure it works.
-
