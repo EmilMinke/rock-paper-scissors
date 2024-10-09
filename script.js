263722c6ba12c92ff70c5e96bce948f972e18278
EmilMinke <em.minke@gmail.com>
2024-10-09 12:03:40 +0200

    feat: Add the logic to play a single round

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-
 let humanScore = 0;
 let computerScore = 0;
-let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-function getComputerChoice(computerChoice) {
-    return (computerChoice <= 33) ? "Rock" :
-    (computerChoice <= 66 && computerChoice > 33) ? "Paper" :
-    "Scissors";
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    return (randomNumber <= 33) ? "rock" :
+            (randomNumber <= 66) ? "paper" :
+            "scissors";
 }
 
 function getHumanChoice(){
-    let userInput = prompt("Rock, Paper or Scissors?");
-    return userInput;
+    const humanChoice = prompt("Rock, Paper or Scissors?");
+    return humanChoice;
 }
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! Both chose ${humanChoice}.`)
+        return;
+    }
+
+    if ((humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        console.log(`You win! ${humanChoice.at(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}!`);
+    }
+    else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice.at(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}!`);
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+console.log(`Scores - Human: ${humanScore}, Computer: ${computerScore}`);
 
 // Plan or pseudocode your solution.
 // Write the code.
